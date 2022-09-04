57809389990545f46ab23783aca01f645e29a967
tamaslud <tamaslud@yahoo.com>
2022-09-04 15:32:08 +0200

    content filled by JS

--- js/images.js
+++ js/images.js
@@ -1,27 +1,27 @@
 const gallery = [
 {
     id: 1,
-    src: './css/images.Grossglockner.jpg',
+    src: './css/images/Grossglockner.jpg',
     alt: 'Grossglockner'
 },
 {
     id: 2,
-    src: './css/images.Marmolada.jpg',
+    src: './css/images/Marmolada.jpg',
     alt: 'Marmolada'
 },
 {
     id: 3,
-    src: './css/images.Montblanc.jpg',
+    src: './css/images/Montblanc.jpg',
     alt: 'Mont Blanc'
 },
 {
     id: 4,
-    src: './css/images.Trecime.jpg',
+    src: './css/images/Trecime.jpg',
     alt: 'Tre Cime di Lavaredo'
 },
 {
     id: 5,
-    src: './css/images.Vysoka.jpg',
+    src: './css/images/Vysoka.jpg',
     alt: 'Tátra-csúcs (Vysoka)'
 },
 ];
--- js/main.js
+++ js/main.js
@@ -1,7 +1,31 @@
 import gallery from './images.js';
+
 const imagesTotal = gallery.length;
-console.log('összes kép:' + imagesTotal);
-gallery.forEach(element => {
-    console.log(element);
-    
-});
+let actualImageIndex = 4;
+
+const imageCounter = document.querySelector('.imageCounter');
+const focusedImage = document.querySelector('.focusedImage');
+const imageDescription = document.querySelector('.imageDescription');
+const buttonBox = document.querySelector('.buttonBox');
+
+let actualImageObject;
+let focusedImageHTML;
+
+const btnBoxActual = `
+<img class="radio" src="./css/icons/radio_button_checked_FILL0_wght400_GRAD0_opsz48.svg" alt="">
+<img class="radio" src="./css/icons/radio_button_unchecked_FILL0_wght400_GRAD0_opsz48.svg" alt="">
+<img class="radio" src="./css/icons/radio_button_unchecked_FILL0_wght400_GRAD0_opsz48.svg" alt="">
+<img class="radio" src="./css/icons/radio_button_unchecked_FILL0_wght400_GRAD0_opsz48.svg" alt="">
+<img class="radio" src="./css/icons/radio_button_unchecked_FILL0_wght400_GRAD0_opsz48.svg" alt="">
+`
+
+const displayContent = (actualImageIndex) => {
+    actualImageObject = gallery[actualImageIndex];
+    focusedImageHTML = `<img src="${actualImageObject.src}" alt="${actualImageObject.alt}">`
+    focusedImage.innerHTML = focusedImageHTML;
+    imageDescription.innerHTML = actualImageObject.alt;
+    imageCounter.innerHTML = `${actualImageIndex+1}/${imagesTotal}`;
+    buttonBox.innerHTML = btnBoxActual;
+}
+
+displayContent(actualImageIndex);
