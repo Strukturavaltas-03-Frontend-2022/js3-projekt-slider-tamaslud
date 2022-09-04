a2b09ba0982b4e706b8f059f9a059cd44538dbb4
tamaslud <tamaslud@yahoo.com>
2022-09-04 13:31:22 +0200

    fonts styles

---
+++ js/main.js
@@ -0,0 +1,7 @@
+import gallery from './images.js';
+const imagesTotal = gallery.length;
+console.log('összes kép:' + imagesTotal);
+gallery.forEach(element => {
+    console.log(element);
+    
+});
